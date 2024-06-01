520871ac6d37f6397b10fa79ed60404883eda4cd
VladimirPutinHero <160325494+VladimirPutinHero@users.noreply.github.com>
2024-06-01 10:53:11 +0300

    Update test.py

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ from lab_4 import *
 def test_create_snapshot(mock_listdir):
     # Arrange
     folder_path = 'C:/Users/PC/Downloads/python/snapshots'
-    snapshot_path = 'C:/Users/PC/Downloads/python'
+    snapshots_path = 'C:/Users/PC/Downloads/python'
     mock_listdir.return_value = ['file1.txt', 'file2.py', 'file3.jpg']
 
     # Act
